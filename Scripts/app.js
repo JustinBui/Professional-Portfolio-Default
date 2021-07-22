5c3391ec637885a3b662d1086709ddc78a0b674b
BuiTheBoi <uNUKEcorn15@gmail.com>
2021-07-22 01:54:24 -0700

    Implemented a functioning navbar using JQuery :rocket: :alien:

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,53 +1,12 @@
-// -------------------------------------------------------------------------
-// Tutorial for function: https://www.youtube.com/watch?v=oUSvlrDTLi4
-// -------------------------------------------------------------------------
+// JQuery Smooth Scroll when clicking on navbar buttons
+// Reference: https://www.youtube.com/watch?v=y9nlfqT4s9s
+$(".navbar a").on("click", function (e) {
+    if (this.hash !== "") {
+        e.preventDefault(); // Preventing the default behavior when clicking buttons
+        const hash = this.hash;
 
-
-// function smoothScroll(target, duration) {
-//     var targetElement = document.querySelector(target);
-//     var targetPosition = targetElement.getBoundingClientRect().top; // Getting distance (in px) relative to the top of certain element
-//     var startPosition = window.pageYOffset;  // Getting distance (in px) relative to your window
-//     var distance = targetPosition - startPosition;
-//     var startTime = null;
-
-//     function animation(currentTime) {
-//         if (startTime === null) startTime = currentTime;
-//         console.log(startTime);
-//         var timeElapsed = currentTime - startTime;
-//         var run = ease(timeElapsed, startPosition, distance, duration);
-//         window.scrollTo(0, run)  // NO SCROLLS for x axis (first param), but have an animation scroll for y axis (second param)
-//         if (timeElapsed < duration) requestAnimationFrame(animation);
-//     }
-
-//     // Function for smooth transition
-//     function ease(t, b, c, d) {
-//         t /= d / 2;
-//         if (t < 1) return c / 2 * t * t + b;
-//         t--;
-//         return -c / 2 * (t * (t - 2) - 1) + b;
-//     }
-
-//     requestAnimationFrame(animation);   // Calls animation and loops through it @ 60 FPS
-// }
-
-
-
-// Event listeners
-// let homeButton = document.querySelector("#home-btn");
-// let aboutButton = document.querySelector("#abt-btn");
-// let skillButton = document.querySelector("#skill-btn");
-// let latestWorkButton = document.querySelector("#latest-wrk-btn");
-
-
-// window.scrollTo() approach
-function smoothScroll(event) {
-    const targetId = event.currentTarget.getAttribute("href");
-    console.log(targetId);
-    window.scrollTo({
-        top: document.querySelector(targetId).offsetTop,    // Getting distance from top
-        behavior: "smooth"
-    });
-}
-
-const navLinks = document.querySelectorAll(".navbar a");
-navLinks.forEach(elem => elem.addEventListener("click", smoothScroll));
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 800);    // scrolling from the top taking 800 ms
+    }
+});

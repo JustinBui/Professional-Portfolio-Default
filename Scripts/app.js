b67ee2e8fac0dffb63cd928951fc82e0f1c99cfb
BuiTheBoi <uNUKEcorn15@gmail.com>
2021-07-23 02:06:33 -0700

    Added scroll to contacts :palm_tree: :money_with_wings:
    Though the Javascript is may not be efficient, I will find ways to clean up the code the more I learn about JavaScript.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,6 +1,9 @@
-// JQuery Smooth Scroll when clicking on navbar buttons
 // Reference: https://www.youtube.com/watch?v=y9nlfqT4s9s
-$(".navbar a").on("click", function (e) {
+// Using JQuery for smooth scroll animations
+
+
+// Clicking navbar buttons
+$(".button").on("click", function (e) {
     if (this.hash !== "") {
         e.preventDefault(); // Preventing the default behavior when clicking buttons
         const hash = this.hash;
@@ -8,5 +11,18 @@ $(".navbar a").on("click", function (e) {
         $("html, body").animate({
             scrollTop: $(hash).offset().top
         }, 800);    // scrolling from the top taking 800 ms
+    }
+});
+
+// Clicking link saying "Contact Me Below!" in About Me section
+$(".scroll-to-contacts").on("click", function (e) {
+    console.log(this.hash);
+    if (this.hash !== "") {
+        e.preventDefault();
+        const hash = this.hash;
+
+        $("html, body").animate({
+            scrollTop: $(hash).offset().top
+        }, 800);
     }
 });
